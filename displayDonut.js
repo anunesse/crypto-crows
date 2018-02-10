bebdf1606f4e1668aeddeddee40d81630090005d
Alexandre Nunesse <alexandre.nunesse@azeti.net>
2018-02-10 16:50:35 +0100

    Add mocked bar chart

--- displayDonut.js
+++ displayDonut.js
@@ -1,26 +1,12 @@
-window.onload = () => {
+drawChart = (data) => {
 
 	const chart = new CanvasJS.Chart("chartContainer", {
-		animationEnabled: true,
 		title:{
-			text: "Email Categories",
-			horizontalAlign: "left"
+			text: "Data"
 		},
 		data: [{
-			type: "doughnut",
-			startAngle: 60,
-			//innerRadius: 60,
-			indexLabelFontSize: 17,
-			indexLabel: "{label} - #percent%",
-			toolTipContent: "{label}: {y} (#percent%)",
-			dataPoints: [
-				{ y: 67, label: "Inbox" },
-				{ y: 28, label: "Archives" },
-				{ y: 10, label: "Labels" },
-				{ y: 7, label: "Drafts"},
-				{ y: 15, label: "Trash"},
-				{ y: 6, label: "Spam"}
-			]
+			type: "column",
+			dataPoints: data
 		}]
 	});
 
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const request =
 	'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH,BTC&tsyms=EUR';
+const data = [
+	{ y: 67, label: "Inbox" },
+	{ y: 28, label: "Archives" },
+	{ y: 10, label: "Labels" },
+	{ y: 7, label: "Drafts"},
+	{ y: 15, label: "Trash"},
+	{ y: 6, label: "Spam"}
+];
 
 fetch(request)
 	.then(response => response.json())
 	.then(response => {
+		drawChart(data);
 		const app = new Vue({
 			el: '#app',
 			data: {
