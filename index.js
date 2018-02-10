3f0349368fc9b461737d3c3d8f617c05a66ee10f
Alexandre Nunesse <alexandre.nunesse@azeti.net>
2018-02-10 15:53:36 +0100

    Load data

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.info('Hello world !');
+const request =
+	'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH,BTC&tsyms=EUR';
+
+fetch()
+	.then(response => response.json())
+	.then(response => {
+		console.info(response);
+	});
