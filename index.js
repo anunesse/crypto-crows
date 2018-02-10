8cc4924607996d8724f0017c8b9422a5c90cbb1e
Alexandre Nunesse <alexandre.nunesse@azeti.net>
2018-02-10 16:07:19 +0100

    Add vuejs and display eth, btc

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const request =
 	'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH,BTC&tsyms=EUR';
 
-fetch()
+fetch(request)
 	.then(response => response.json())
 	.then(response => {
-		console.info(response);
+		const app = new Vue({
+			el: '#app',
+			data: {
+				result: response.DISPLAY
+			}
+		});
 	});
